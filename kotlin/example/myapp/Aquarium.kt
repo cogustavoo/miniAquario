238f0f360ed24740b26c18a1b245a14b67893595
Gustavo Correa <gustavocorrea66@gmail.com>
2021-12-28 17:06:22 -0300

    utilizando get and set

--- kotlin/example/myapp/Aquarium.kt
+++ kotlin/example/myapp/Aquarium.kt
@@ -2,10 +2,32 @@ package example.myapp
 // criando uma class com constructor  voce pode definir argunmentos e pegar argumentos default
 // ou criar aquarios com sizes diferentes
 class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+    //init faz com que esses blocos se iniciem no execute
+    init {
+        println(" aquarium initializing")
+    }
+    var volume: Int
+        get() = width * height * length / 1000  // 1000 cm^3 = 1 l
+        // adicionando um set para recalcular o height baseado no montante de agua VALUE e o valor convencional do set
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
+
+
+    constructor(numberOfFish: Int) : this() {
+        // 2000 cn3 per fish + espaco extra para a agua n transbordar
+        val tank = numberOfFish * 2000 * 1.1
+        // calcula o tamanho necessario
+        height = (tank / (length * width)).toInt()
+    }
 
     fun printSize() {
         println("width: $width cm " +
                 "Length: $length cm" +
                 "Height: $height cm")
+        // 1 l = 1000 cm^3
+        println("Volume: $volume l")
     }
+
+
 }
--- kotlin/example/myapp/main.kt
+++ kotlin/example/myapp/main.kt
@@ -1,17 +1,11 @@
 package example.myapp
 
 fun buildAquarium() {
-    val aquarium1 = Aquarium()
-    aquarium1.printSize()
-    // apenas width custom resto default
-    val aquarium2 = Aquarium(width = 25)
-    aquarium2.printSize()
-    // apenas com width default
-    val aquarium3 = Aquarium(height = 35, length = 110)
-    aquarium3.printSize()
-    // todas as variaveis custom
-    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
-    aquarium4.printSize()
+    val aquarium6 = Aquarium(numberOfFish = 29)
+    aquarium6.printSize()
+    aquarium6.volume = 70
+    aquarium6.printSize()
+
 }
 
 fun main () {
